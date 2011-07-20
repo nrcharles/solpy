c36a30c4aee076bd07a169235f41e7640490dfa2
Nathan Charles <ncharles@gmail.com>
2011-07-21 01:26:34 +0600

    cleaned up abstraction

--- fisheye.py
+++ fisheye.py
@@ -26,72 +26,65 @@ def usage():
     """
     print """usage: [options]
    -h    help
-   -f    filename of photo 
+   -f    filename of fisheye panorama 
     """
- 
-# some definitions
-global outputArray 
-gImage = None
 
-#class world
-#physical data
-#token to pass env data through GUI
-#
-class nugget():
+class sky():
     def __init__(self):
         self.center = 0
-        self.res = None
         self.radius = 0
         self.cardinalOffset = 0 
 
-class GUI():
+class controller():
     def __init__(self,cImage):
+        self.sky = sky()
         self.cImage = cImage
+        self.res = cv.GetSize(cImage) 
+        x,y = self.res
+        self.sky.radius = max(self.res)/2 
+        self.sky.center = (x/2,y/2)
+        self.sky.cardinalOffset = 0
         self.threshold = 0
-        res = cv.GetSize(cImage) 
-        self.res = res
-        self.radius = max(self.res)/2 
-        x,y = res
-        self.center = (x/2,y/2)
-        self.threshold = 0
-        self.cardinalOffset = 0
-        windowName = "Edge"
-        # create the window
+        windowName = "Fisheye"
         cv.NamedWindow(windowName,cv.CV_WINDOW_NORMAL)
         cv.NamedWindow('Image')
         cv.MoveWindow('Image',1,220)
      
-        # create the trackbar
         cv.CreateTrackbar ("Threshold", windowName, 1, 100, self.onThresholdChange)
-        cv.CreateTrackbar ("Horizon", windowName, 10, max(res)/2, self.onRadiusChange)
+        cv.CreateTrackbar ("Horizon", windowName, 10, max(self.res)/2, self.onRadiusChange)
         cv.CreateTrackbar ("North", windowName, 1, 360, self.onNorthChange)
         cv.CreateTrackbar ('x', windowName, x/2, x, self.on_Xchange)
         cv.CreateTrackbar ('y', windowName, y/2, y, self.on_Ychange)
-        #cv.CreateButton('save', self.onSave)
         cv.ResizeWindow(windowName, 1200,200)
         self.onThresholdChange (0)
         
     def onThresholdChange(self, pos):
         self.threshold = pos
-        draw(self)
+        self.draw()
      
     def onRadiusChange(self, rad):
-        self.radius = max(self.res)/2 - rad
-        draw(self)
+        self.sky.radius = max(self.res)/2 - rad
+        self.draw()
 
     def onNorthChange(self, offset):
-        self.cardinalOffset = offset
-        draw(self)
+        self.sky.cardinalOffset = offset
+        self.draw()
 
     def on_Xchange(self, newx):
-        x,y = self.center
-        self.center = (newx,y)
-        draw(self)
+        x,y = self.sky.center
+        self.sky.center = (newx,y)
+        self.draw()
 
     def on_Ychange(self, newy):
-        x,y = self.center
-        self.center = (x,newy)
-        draw(self)
+        x,y = self.sky.center
+        self.sky.center = (x,newy)
+        self.draw()
+
+    def draw(self):
+        self.skyArray = skyToPolar(self.cImage,self.sky,self.threshold)
+        self.displayImage = renderSkyArray(self.cImage,self.skyArray,self.sky)
+        self.outputArray = magnitudeToTheta(self.skyArray,self.sky.radius)
+        cv.ShowImage('Image', self.displayImage)
 
     def onSave(self):
         pass
@@ -119,25 +112,33 @@ def pixelCompare(image, p1,p2):
     else:
         return False
 
-def distanceToEdge(image, point, radius, cardinalOffset = 0 ):
-    rx,ry = cv.GetSize(image) 
-    x,y = point
+def skyToPolar(image,sky,threshold):
+    """takes:  image, sky, threshold
+    returns: rendered image, outputArray"""
+    res = cv.GetSize(image) 
+    col_edge = cv.CreateImage(res, 8, 3)
+    cv.SetZero(col_edge)
+    edge = getCannyMask(image,threshold)
+    cv.Copy(image, col_edge, edge)
+    final = drawHorizonLine(col_edge,sky)
+    x,y = sky.center
     #point is in pixel cordinates, but comparision is right hand cordinates
     rPoint = (y,x)
     vectors = []
     for i in range(360):
         j = 0
-        while pixelCompare(image,addVectorR(rPoint,(i,j)),rPoint) and j < radius:
+        while pixelCompare(final,addVectorR(rPoint,(i,j)),rPoint) and j < sky.radius:
             j += 1
-        vectors.append(((720 + 90 - i - cardinalOffset) % 360 ,j))
-    image[y,x+4] = (255,0,255)
-    cv.Circle(image, point, 1, (0,255,0))
-    cv.Circle(image, (x/2,y/2), 1, (255,255,255))
-    #debugging window
-    #cv.NamedWindow('temp')
-    #cv.MoveWindow('temp',800,220)
-    #cv.ShowImage('temp', image)
+        vectors.append(((720 + 90 - i - sky.cardinalOffset) % 360 ,j))
     return vectors
+
+def magnitudeToTheta(array,radius):
+    theta = []
+    for vector in array:
+        a,m = vector
+        t = (float(radius - m)/float(radius)) * 90 
+        theta.append((a,t))
+    return theta
 
 def addVector(point,vector):
     """Warning!: pixel based system
@@ -149,34 +150,14 @@ def addVector(point,vector):
     dy = int(magnitude * sin(radians(angle)))
     return (x + dx, y - dy)
 
-def drawVectors(image,point,array,cardinalOffset = 0):
+def drawVectors(image,array,sky):
     """"given a start point and an array of magnitudes and degree angles
     draws on gImage"""
     for vector in array:
         a,m = vector
-        cv.Line(image, point, addVector(point,(a+cardinalOffset, m)) ,(255,0,0))
+        cv.Line(image, sky.center, addVector(sky.center,(a+sky.cardinalOffset, m)) ,(255,0,0))
     return image
 
-def magnitudeToTheta(array,radius):
-    theta = []
-    for vector in array:
-        a,m = vector
-        t = (float(radius - m)/float(radius)) * 90 
-        theta.append((a,t))
-    return theta
-
-def saveArray(array,filename):
-    #print array
-    f = open(filename,'w')
-    #for value in outputArray:
-    #    f.write('%s,%s\n' % value)
-    f.write('00 Cordinates to come later\n')
-    f.write(','.join([str(v[0]) for v in array]))
-    f.write('\n')
-    f.write(','.join([str(v[1]) for v in array]))
-    f.write('\n')
-    f.flush()
-    f.close()
 
 def getCannyMask(image,threshold):
     """convert image to gray scale run the edge dector 
@@ -188,17 +169,12 @@ def getCannyMask(image,threshold):
     cv.CvtColor(image, gray, cv.CV_BGR2GRAY)
     cv.Smooth (gray, mask, cv.CV_BLUR, 3, 3, 0)
     cv.Not(gray, mask)
- 
     cv.Canny(gray, mask, threshold, threshold * 3, 3)
     return mask
 
-def saveVectorImage(filename,center,vectorArray,res,cardinalOffset = 0):
-    vectorImage = cv.CreateImage(res, 8, 3)
-    cv.Zero(vectorImage)
-    drawVectors(vectorImage,center,vectorArray,cardinalOffset)
-    cv.SaveImage(filename,vectorImage)
-
-def drawHorizonMask(image, center, radius):
+def drawHorizonMask(image, sky):
+    center = sky.center
+    radius = sky.radius
     res = cv.GetSize(image) 
     mask = cv.CreateImage(res, 8, 1)
     cv.Zero(mask)
@@ -210,36 +186,43 @@ def drawHorizonMask(image, center, radius):
     cv.Copy(image, reimage, mask)
     return reimage 
 
-def drawHorizonLine(image, center, radius):
-    cv.Circle(image, center, radius, (255,255,255))
+def drawHorizonLine(image, sky):
+    cv.Circle(image, sky.center, sky.radius, (255,255,255))
     return image
 
-def drawNorth(image, point, radius, offset):
+def drawNorth(image, sky):
+    point = sky.center
+    radius = sky.radius
+    offset = sky.cardinalOffset
     cv.Line(image, point, addVector(point,(offset,radius)), (0,0,255))
     cv.Line(image, point, addVector(point,((offset+180) % 360,radius)), (255,255,255))
     return image
 
-def draw(env):
-    res = cv.GetSize(env.cImage) 
-    col_edge = cv.CreateImage(res, 8, 3)
-    cv.SetZero(col_edge)
-    edge = getCannyMask(env.cImage,env.threshold)
-    cv.Copy(env.cImage, col_edge, edge)
-    final = drawHorizonLine(col_edge,env.center,env.radius)
-    skyArray = distanceToEdge(final,env.center,env.radius,env.cardinalOffset)
-
+def renderSkyArray(image,skyArray,sky):
+    res = cv.GetSize(image) 
     displayImage = cv.CreateImage(res, 8, 3)
     cv.SetZero(displayImage)
-    cv.Copy(env.cImage, displayImage)
-    displayImage = drawVectors(displayImage, env.center, skyArray, env.cardinalOffset)
-    displayImage = drawNorth(displayImage,env.center,env.radius, env.cardinalOffset)
-    displayImage = drawHorizonLine(displayImage,env.center, env.radius)
+    cv.Copy(image, displayImage)
+    displayImage = drawVectors(displayImage,skyArray, sky)
+    displayImage = drawNorth(displayImage, sky)
+    displayImage = drawHorizonLine(displayImage,sky)
+    return displayImage
 
-    env.outputArray = magnitudeToTheta(skyArray,env.radius)
-    env.displayImage = displayImage
+def saveArray(array,filename):
+    f = open(filename,'w')
+    f.write('00 Cordinates to come later\n')
+    f.write(','.join([str(v[0]) for v in array]))
+    f.write('\n')
+    f.write(','.join([str(v[1]) for v in array]))
+    f.write('\n')
+    f.flush()
+    f.close()
 
-    cv.ShowImage('Image', displayImage)
-
+def saveVectorImage(filename,vectorArray,res,sky):
+    vectorImage = cv.CreateImage(res, 8, 3)
+    cv.Zero(vectorImage)
+    drawVectors(vectorImage,vectorArray,sky)
+    cv.SaveImage(filename,vectorImage)
 
 if __name__ == "__main__":
     import getopt
@@ -264,7 +247,7 @@ if __name__ == "__main__":
             print "Error loading image '%s'" % filename
             sys.exit(-1) 
 
-        ui = GUI(gImage)
+        ui = controller(gImage)
         cv.WaitKey (0)
         ui.save(os.path.basename(filename).split('.')[0])
 
