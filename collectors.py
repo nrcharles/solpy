4733436cbc3f12ddeb4534788a3253337ae6022d
Daniel Thomas <DanielErikThomas@gmail.com>
2012-11-08 21:19:38 +0600

    Corrected Intercepted_Tang(), added calcs for T-type collectors

--- collectors.py
+++ collectors.py
@@ -2,6 +2,7 @@
 # collector.py
 #   This file contains solar thermal collector classes used by thermal.py
 #   Specifications for specific collectors are also here for now.
+pi = 3.141
 
 class tube(object):
     '''Double-wall vacuum glass tube collectors.  Two main sizes are defined, the
@@ -16,8 +17,10 @@ class tube(object):
         self.L_tot = 1.816              # Overall length [m]
     def v47mm(self):
         pass
-    def volume(self):
-        return 3.141*(self.D1**2)*self.L_ab
+    def exposed_area(self):             # Outer surface area, exposed to air [m2]
+        return pi*self.D1*self.L_ab
+    def volume(self):                   # Interior volume, capacity [m3]
+        return pi*(self.D1**2)*self.L_ab
 
 class collector(object):
     '''Currently written for vacuum tube collectors; needs to extended to flat plate collectors'''
@@ -25,14 +28,18 @@ class collector(object):
         pass
     def manifold_area(self):            # Surface area, excluding tube holes
         c = self.__class__
-        return (2*c.w + 2*c.h)*c.L + 2*(c.w*c.h) - c.N_tubes*(3.141*((c.t.D2)**2))
+        return (2*c.w + 2*c.h)*c.L + 2*(c.w*c.h) - c.N_tubes*(pi*((c.t.D2)**2))
+    def tube_area(self):                # Total outer surface area of tubes [m2]
+        c = self.__class__
+        return c.t.exposed_area() * c.N_tubes
 
 class collectorArray(collector):
-    def __init__(self,coll,series=1,parallel=1,beta=0,gamma=0):
-        self.c     = coll
+    def __init__(self,coll,series=1,parallel=1,slope=0,azimuth=0,DFR=False):
+        self.c     = coll               # Collector object
         self.N     = series*parallel    # Currently only considering total size
-        self.beta  = beta               # Collector slope
-        self.gamma = gamma              # Collector azimuth angle (0 = due South)
+        self.beta  = slope*pi/180       # Collector slope [radians]
+        self.gamma = azimuth*pi/180     # Collector azimuth [radians] (0 = due South)
+        self.DFR   = DFR                # Diffuse Flat Reflector [T/F] behind tubes
         self.A_gr  = self.c.A_gr*self.N
         self.V     = self.c.V   *self.N
         self.N_tubes=self.c.N_tubes*self.N
@@ -40,6 +47,8 @@ class collectorArray(collector):
         self.b1,b2,b3= 0.0,0.0,0.0        
     def manifold_area(self):
         return self.c.manifold_area()*self.N
+    def tube_area(self):
+        return self.c.tube_area()*self.N
     def Q_out(self,H,dT):
         '''Daily total collector heat output from ISO 9459-2 collector equation: 
              Q_u = a1*H + a2*(T_i-T_amb) + a3
@@ -51,6 +60,7 @@ class collectorArray(collector):
         return (self.N_tubes/self.c.N_tubes)*(self.b1*Qt+ self.b2*dT + self.b3)
 
 class chuanghui_H50(collector):
+    type  = 'H'
     A_gr  = 2*1.87*1.70                 # [m2] Collector gross tube area (6.358 m2, frame = 2*1.93*1.885)
     V     = 0.145                       # [m3] Collector volume
     B     = 0.075                       # [m]  Tube spacing (center to center)
