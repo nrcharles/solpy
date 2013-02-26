9e62163a538de24594a2315a875ba14e9f94f530
Nathan Charles <ncharles@gmail.com>
2013-02-26 13:26:22 -0500

    fix factors

--- tools.py
+++ tools.py
@@ -12,6 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 def factors(n):    
     #http://stackoverflow.com/questions/6800193/
     return set(reduce(list.__add__,
