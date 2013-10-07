1a57ba863fc23655ee1704a1981c2c2e0d586ac3
Nathan Charles <ncharles@gmail.com>
2013-10-07 15:49:52 -0400

    licensing details

--- geo.py
+++ geo.py
@@ -1,18 +1,6 @@
-#    Copyright 2012 Nathan Charles
+# Copyright (C) 2013 Nathan Charles
 #
-#
-#    This is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This software is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with solpy. If not, see <http://www.gnu.org/licenses/>.
+# This program is free software. See terms in LICENSE file.
 
 import csv
 import math
