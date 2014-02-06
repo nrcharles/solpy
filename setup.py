142e08e34d8db555e5e87e377d163a0219d38638
Nathan Charles <ncharles@gmail.com>
2014-02-06 10:23:16 -0500

    setuptools

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
-from distutils.core import setup
-setup(name='solpy',
-      version='0.1',
-      url="https://github.com/nrcharles/solpy",
-      package_dir={'solpy': ''},
-      package_data={'solpy': ['*.json','*.csv','data.fdf']},
-      packages=['solpy'],
-      requires=['numpy','matplotlib','geopy']
-      )
+import os
+from setuptools import setup,find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "solpy",
+    version = "0.1",
+    author = "Nathan Charles",
+    author_email = "ncharles@gmail.com",
+    description = ("Solar Performance and Design library"),
+    license = "LPGL",
+    keywords = "solar pv",
+    url = "https://github.com/nrcharles/solpy",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    install_requires = ['geopy','numpy','matplotlib'],
+    package_data={'': ['*.json','*.csv','data.fdf']},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+    ],
+)
