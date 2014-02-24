5f3c048204db4669d8060dd93ab81dacce0aa275
Nathan Charles <ncharles@gmail.com>
2014-02-24 08:22:58 -0500

    require scipy

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name = "solpy",
-    version = "0.1.7",
+    version = "0.1.8",
     author = "Nathan Charles",
     author_email = "ncharles@gmail.com",
     description = ("Solar Performance and Design library"),
@@ -17,7 +17,7 @@ setup(
     url = "https://github.com/nrcharles/solpy",
     packages=find_packages(),
     long_description=read('README'),
-    install_requires = ['geopy','numpy','matplotlib'],
+    install_requires = ['geopy','numpy','matplotlib','scipy'],
     package_data={'': ['*.json','*.csv','data.fdf']},
     classifiers=[
         "Development Status :: 4 - Beta",
