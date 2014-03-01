72db539a139e1be1c7a4e67240df737ca3e9b4f9
Nathan Charles <ncharles@gmail.com>
2014-03-01 14:09:27 -0500

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name = "solpy",
-    version = "0.1.8",
+    version = "0.1.9",
     author = "Nathan Charles",
     author_email = "ncharles@gmail.com",
     description = ("Solar Performance and Design library"),
