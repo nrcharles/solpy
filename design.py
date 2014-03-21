226a41386e160dd39f57b8016537da16f85f0d9b
Nathan Charles <ncharles@gmail.com>
2014-03-21 16:55:42 -0400

    knapsack algorithm to build system

--- design.py
+++ design.py
@@ -1,8 +1,12 @@
 import copy
 import geo
 import epw
+import pv
+import json
 
-def fill(inverter, zipcode, acDcRatio = 1.2, mount="Roof", stationClass = 1, Vmax = 600, bipolar= True):
+
+def fill(inverter, zipcode, acDcRatio = 1.2, mount="Roof", stationClass = 1, \
+        Vmax = 600, bipolar= True):
     """maximize array"""
     tDerate = {"Roof":30,
             "Ground":25,
@@ -11,7 +15,8 @@ def fill(inverter, zipcode, acDcRatio = 1.2, mount="Roof", stationClass = 1, Vma
     maxV = inverter.array.panel.Vmax(epw.minimum(usaf))
     #NREL suggests that long term degradation is primarily current not voltage
     derate20 = .97
-    minV = inverter.array.panel.Vmin(epw.twopercent(usaf),tDerate[mount]) * derate20
+    minV = inverter.array.panel.Vmin(epw.twopercent(usaf),tDerate[mount]) * \
+            derate20
     #print "MinV", minV
     if inverter.vdcmax != 0:
          Vmax = inverter.vdcmax
@@ -39,20 +44,102 @@ def fill(inverter, zipcode, acDcRatio = 1.2, mount="Roof", stationClass = 1, Vma
                             solutions.append(t)
     return solutions
 
-def design(DCsize, panels, inverters):
-    designs = []
-    for i in inverters:
-        for i in panels:
-            #expedite.
-            pass
+def knapsack(items, maxweight):
+    #http://codereview.stackexchange.com/questions/20569/dynamic-programming-solution-to-knapsack-problem
+    bestvalues = [[0] * (maxweight + 1)
+                  for i in xrange(len(items) + 1)]
+
+    for i, (value, weight, systemDict) in enumerate(items):
+        i += 1
+        for capacity in xrange(maxweight + 1):
+            if weight > capacity:
+                bestvalues[i][capacity] = bestvalues[i - 1][capacity]
+            else:
+                candidate1 = bestvalues[i - 1][capacity]
+                candidate2 = bestvalues[i - 1][capacity - weight] + value
+                bestvalues[i][capacity] = max(candidate1, candidate2)
+
+    reconstruction = []
+    i = len(items)
+    j = maxweight
+    while i > 0:
+        if bestvalues[i][j] != bestvalues[i - 1][j]:
+            reconstruction.append(items[i - 1])
+            j -= items[i - 1][1]
+        i -= 1
+
+    reconstruction.reverse()
+
+    return bestvalues[len(items)][maxweight], reconstruction
+
+def design(reqsStr):
+#def design(DCsize, panellist, inverterlist, details):
+    """parts selection algorithm"""
+    #create all valid inverter panel combinations for location
+    reqs = json.loads(reqsStr)
+    validC = []
+    clean = []
+    zc = reqs['zipcode']
+    #for inverterModel,inverterCost in inverterlist:
+    for inverterModel in reqs['inverter options']:
+        #for panelModel,panelPrice in panellist:
+        for panelModel in reqs['panel options']:
+            system = inverters.inverter(inverterModel,\
+                    modules.pvArray(modules.module(panelModel),[2]))
+            configs = fill(system,zc)
+            for config in configs:
+                validC.append(config)
+                print config, config.array, config.array.panel, \
+                        config.array.output(1000), config.ratio()
+
+                reqs['array'] = [config.dump()]
+                plant = pv.jsonToSystem(reqs)
+                output = plant.model()
+                print "year 1 kwh", output.annualOutput
+                config.yearOne = output.annualOutput
+
+                #value, weight, systemDict
+                option = [(int(config.yearOne), \
+                        int(config.array.output(1000)), config.dump())]
+
+                #hack to allow semetery
+                scale = int(reqs['desired size'] / config.array.output(1000))
+                clean += option * scale
+
+    #knapsack problem weight is system DC size and value is annual output
+    #this could be expanded with different constraints for different rankings
+    systemWeight, systemSet = knapsack(clean, reqs['desired size'])
+
+    reqs['array'] = [subA for v,w,subA in systemSet]
+    return reqs
 
 if __name__ == "__main__":
     import inverters
     import modules
-    zc='27713'
-    m = "Mage Solar : USA Powertec Plus 250-6 MNCS"
-    ms = modules.module(m)
-    system = inverters.inverter("Refusol: 20 kW 480V",modules.pvArray(ms,[11]*6))
-    s = fill(system,zc)
-    for i in s:
-        print i, i.ratio()
+    import expedite
+
+    #sketchup may be a good way to input this data
+    testreqs = """{"system_name":"HAPPY CUSTOMER",
+        "address":"15013 Denver W Pkwy, Golden, CO",
+        "zipcode":"80401",
+        "phase":1,
+        "voltage":240,
+        "service":200,
+        "tilt":25,
+        "azimuth":180,
+        "inverter options":["SMA America: SB4000TL-US-22 (240V) 240V", 
+            "SMA America: SB5000TL-US-22 (240V) 240V", 
+            "SMA America: SB6000US-11 240V"],
+        "panel options":["Axitec : AC-250P-156-60S *"],
+        "homerun":150,
+        "space":[[10,5]],
+        "desired size":130000}"""
+    shade = """{
+        "shade":{"0": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "1": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.6, 1.0, 0.2, 0.0, 0.1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "2": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.6, 1.0, 1.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "3": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "4": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.9, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "5": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "6": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "7": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "8": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 1.0, 1.0, 0.1, 0.0, 0.6, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "9": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.9, 1.0, 0.1, 0.0, 0.1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "10": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 1.0, 1.0, 1.0, 1.0, 0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "11": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 1.0, 1.0, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}
+            }"""
+
+    proposed = design(testreqs)
+    proposedPlant = pv.jsonToSystem(proposed)
+    print json.dumps(proposedPlant.dump(), sort_keys=True, indent=4, \
+            separators=(',', ': '))
+    expedite.string_notes(proposedPlant)
