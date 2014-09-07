179a48b2ec7c6074b7c2136c8f3080139a998fc2
Nathan Charles <ncharles@gmail.com>
2014-09-07 10:47:46 -0400

    mock

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,20 @@
 
 import sys
 import os
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['pygtk','pyephem' 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+
 #sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
