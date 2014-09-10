13ec74e60a07443a9a33896cab1570fd6ecd960a
Nathan Charles <ncharles@gmail.com>
2014-09-10 10:22:00 -0400

    pylint cleanup

--- setup.py
+++ setup.py
@@ -1,31 +1,33 @@
+"""install"""
 import os
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 import datetime
-"""setup.py and README must be copied to parent directory in order to build
-a valid setuptools package"""
 
 NOW = datetime.datetime.now()
 MAJOR = 0
 
 def read(fname):
+    """README helper fuction"""
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "solpy",
-    version = "%s.%s.%s.%s" % (MAJOR, NOW.month, NOW.day, NOW.hour),
-    author = "Nathan Charles",
-    author_email = "ncharles@gmail.com",
-    description = ("Solar Performance and Design library"),
-    license = "LGPL",
-    keywords = "solar pv",
-    url = "https://github.com/nrcharles/solpy",
+    name="solpy",
+    version="%s.%s.%s.%s" % (MAJOR, NOW.month, NOW.day, NOW.hour),
+    author="Nathan Charles",
+    author_email="ncharles@gmail.com",
+    description=("Solar Performance and Design library"),
+    license="LGPL",
+    keywords="solar pv",
+    url="https://github.com/nrcharles/solpy",
     packages=find_packages(),
     long_description=read('README.md'),
-    install_requires = ['geopy','numpy','matplotlib','scipy','pyephem','mock'],
-    package_data={'': ['*.json','*.csv','pvcli']},
+    install_requires=['geopy', 'numpy', 'matplotlib', 'scipy', 'pyephem', \
+            'mock'],
+    package_data={'': ['*.json', '*.csv', 'pvcli']},
     test_suite='tests.unit',
     classifiers=[
         "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        ("License :: OSI Approved ::" \
+            " GNU Library or Lesser General Public License (LGPL)"),
     ],
 )
